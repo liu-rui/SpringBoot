a2c9c8b14131ebc9a58f424f9aafd4e93ed83653
1313475@qq.com <1313475@qq.com>
2016-03-31 11:14:41 +0800

    增加了单元测试

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.myhpc365.dns.Application;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(Application.class)
+public class AppTest { 
+}
